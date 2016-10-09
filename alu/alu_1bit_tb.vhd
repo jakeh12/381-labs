ed933de4493bdd849ebad6de7be450c0f7aac39d
dmlarson <dmlarson@iastate.edu>
2016-10-08 22:45:32 -0500

    fixed test bench

--- alu/alu_1bit_tb.vhd
+++ alu/alu_1bit_tb.vhd
@@ -2,16 +2,6 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity alu_1bit_tb is
-    port(
-    iA      : in std_logic;
-    iB      : in std_logic;
-    iBinv   : in std_logic;
-    iC      : in std_logic;
-    sel     : in std_logic_vector(2 downto 0);
-    less    : in std_logic;
-    oC      : out std_logic;
-    oRe     : out std_logic
-    );
 end alu_1bit_tb;
 
 architecture dataflow of alu_1bit_tb is
@@ -31,7 +21,7 @@ component alu_1bit is
     );
 end component;
 
-signal a, b, inv,s_ic,s_less,s_oc, ores : std_logic;
+signal a, b, inv,s_ic,s_less,s_oc, ores : std_logic := '0';
 signal s : std_logic_vector(2 downto 0);
 
 begin
