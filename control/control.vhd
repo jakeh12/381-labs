37214ad2bc3466b1782a00cc531d66ba842ff01b
dmlarson <dmlarson@iastate.edu>
2016-11-05 22:51:49 -0500

    CONTROL COMPILED GUYS!

--- control/control.vhd
+++ control/control.vhd
@@ -24,11 +24,6 @@ end entity mips_control;
 -------------------------------------------------------------------------------
 architecture rom of mips_control is
 
-  --roms
-  --type rom_rtype is std_logic_vector(23 downto 0); --the size of all output bits
-  --type rom_branch is std_logic_vector(23 downto 0); --the size of all output bits
-  --type rom_other is std_logic_vector(23 downto 0); --the size of all output bits
-
   -- checkout MIPS instruction formats (p. 120)
   alias a_op             : std_logic_vector (5 downto 0) is i_instruction(31 downto 26);
   alias a_rs             : std_logic_vector (4 downto 0) is i_instruction (25 downto 21);
@@ -41,78 +36,79 @@ architecture rom of mips_control is
   alias a_target_address : std_logic_vector (25 downto 0) is i_instruction (25 downto 0);
 
   --OP CODES
-  constant OP_MUL    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_ADD    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_ADDU   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SUB    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SUBU   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_ADDI   : std_logic_vector (5 downto 0) := "001100";
-  constant OP_ADDIU  : std_logic_vector (5 downto 0) := "001001";
-  constant OP_OR     : std_logic_vector (5 downto 0) := "000000";
-  constant OP_ORI    : std_logic_vector (5 downto 0) := "001101";
-  constant OP_AND    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_ANDI   : std_logic_vector (5 downto 0) := "001100";
-  constant OP_XOR    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_XORI   : std_logic_vector (5 downto 0) := "001110";
-  constant OP_NOR    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SLT    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SLTU   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SLTI   : std_logic_vector (5 downto 0) := "001010";
-  constant OP_SLTIU  : std_logic_vector (5 downto 0) := "001011";
-  constant OP_BEQ    : std_logic_vector (5 downto 0) := "000100";
-  constant OP_BNE    : std_logic_vector (5 downto 0) := "000101";
-  constant OP_BLTZ   : std_logic_vector (5 downto 0) := "000001";
-  constant OP_BGEZ   : std_logic_vector (5 downto 0) := "000001";
-  constant OP_BLTZAL : std_logic_vector (5 downto 0) := "000001";
-  constant OP_BGEZAL : std_logic_vector (5 downto 0) := "000001";
-  constant OP_BLEZ   : std_logic_vector (5 downto 0) := "000110";
-  constant OP_BGTZ   : std_logic_vector (5 downto 0) := "000111";
-  constant OP_J      : std_logic_vector (5 downto 0) := "000010";
-  constant OP_JAL    : std_logic_vector (5 downto 0) := "000011";
-  constant OP_JR     : std_logic_vector (5 downto 0) := "001000";
-  constant OP_JALR   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_LB     : std_logic_vector (5 downto 0) := "100000";
-  constant OP_LBU    : std_logic_vector (5 downto 0) := "100100";
-  constant OP_LH     : std_logic_vector (5 downto 0) := "100001";
-  constant OP_LHU    : std_logic_vector (5 downto 0) := "100101";
-  constant OP_LW     : std_logic_vector (5 downto 0) := "100011";
-  constant OP_SB     : std_logic_vector (5 downto 0) := "101000";
-  constant OP_SH     : std_logic_vector (5 downto 0) := "101001";
-  constant OP_SW     : std_logic_vector (5 downto 0) := "101011";
-  constant OP_SLL    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SRL    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SRA    : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SLLV   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SRLV   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_SRAV   : std_logic_vector (5 downto 0) := "000000";
-  constant OP_LUI    : std_logic_vector (5 downto 0) := "001111";
+  constant OP_MUL    : natural := 2#000000#;
+  constant OP_ADD    : natural := 2#000000#;
+  constant OP_ADDU   : natural := 2#000000#;
+  constant OP_SUB    : natural := 2#000000#;
+  constant OP_SUBU   : natural := 2#000000#;
+  constant OP_ADDI   : natural := 2#001000#;
+  constant OP_ADDIU  : natural := 2#001001#;
+  constant OP_OR     : natural := 2#000000#;
+  constant OP_ORI    : natural := 2#001101#;
+  constant OP_AND    : natural := 2#000000#;
+  constant OP_ANDI   : natural := 2#001100#;
+  constant OP_XOR    : natural := 2#000000#;
+  constant OP_XORI   : natural := 2#001110#;
+  constant OP_NOR    : natural := 2#000000#;
+  constant OP_SLT    : natural := 2#000000#;
+  constant OP_SLTU   : natural := 2#000000#;
+  constant OP_SLTI   : natural := 2#001010#;
+  constant OP_SLTIU  : natural := 2#001011#;
+  constant OP_BEQ    : natural := 2#000100#;
+  constant OP_BNE    : natural := 2#000101#;
+  constant OP_BLTZ   : natural := 2#000001#;
+  constant OP_BGEZ   : natural := 2#000001#;
+  constant OP_BLTZAL : natural := 2#000001#;
+  constant OP_BGEZAL : natural := 2#000001#;
+  constant OP_BLEZ   : natural := 2#000110#;
+  constant OP_BGTZ   : natural := 2#000111#;
+  constant OP_J      : natural := 2#000010#;
+  constant OP_JAL    : natural := 2#000011#;
+  constant OP_JR     : natural := 2#000000#;
+  constant OP_JALR   : natural := 2#000000#;
+  constant OP_LB     : natural := 2#100000#;
+  constant OP_LBU    : natural := 2#100100#;
+  constant OP_LH     : natural := 2#100001#;
+  constant OP_LHU    : natural := 2#100101#;
+  constant OP_LW     : natural := 2#100011#;
+  constant OP_SB     : natural := 2#101000#;
+  constant OP_SH     : natural := 2#101001#;
+  constant OP_SW     : natural := 2#101011#;
+  constant OP_SLL    : natural := 2#000000#;
+  constant OP_SRL    : natural := 2#000000#;
+  constant OP_SRA    : natural := 2#000000#;
+  constant OP_SLLV   : natural := 2#000000#;
+  constant OP_SRLV   : natural := 2#000000#;
+  constant OP_SRAV   : natural := 2#000000#;
+  constant OP_LUI    : natural := 2#001111#;
 
 
   --FUNCTION CODES
-  constant FUNC_MUL  : std_logic_vector (5 downto 0) := "111111";
-  constant FUNC_ADD  : std_logic_vector (5 downto 0) := "100000";
-  constant FUNC_ADDU : std_logic_vector (5 downto 0) := "100001";
-  constant FUNC_SUB  : std_logic_vector (5 downto 0) := "100010";
-  constant FUNC_SUBU : std_logic_vector (5 downto 0) := "100011";
-  constant FUNC_OR   : std_logic_vector (5 downto 0) := "100101";
-  constant FUNC_AND  : std_logic_vector (5 downto 0) := "001101";
-  constant FUNC_XOR  : std_logic_vector (5 downto 0) := "100110";
-  constant FUNC_NOR  : std_logic_vector (5 downto 0) := "100111";
-  constant FUNC_SLT  : std_logic_vector (5 downto 0) := "101010";
-  constant FUNC_SLTU : std_logic_vector (5 downto 0) := "101001";
-  constant FUNC_JALR : std_logic_vector (5 downto 0) := "001001";
-  constant FUNC_SLL  : std_logic_vector (5 downto 0) := "000000";
-  constant FUNC_SRL  : std_logic_vector (5 downto 0) := "000010";
-  constant FUNC_SRA  : std_logic_vector (5 downto 0) := "000011";
-  constant FUNC_SLLV : std_logic_vector (5 downto 0) := "000100";
-  constant FUNC_SRLV : std_logic_vector (5 downto 0) := "000110";
-  constant FUNC_SRAV : std_logic_vector (5 downto 0) := "000111";
+  constant FUNC_MUL  : natural := 2#111111#;
+  constant FUNC_ADD  : natural := 2#100000#;
+  constant FUNC_ADDU : natural := 2#100001#;
+  constant FUNC_SUB  : natural := 2#100010#;
+  constant FUNC_SUBU : natural := 2#100011#;
+  constant FUNC_OR   : natural := 2#100101#;
+  constant FUNC_AND  : natural := 2#001101#;
+  constant FUNC_XOR  : natural := 2#100110#;
+  constant FUNC_NOR  : natural := 2#100111#;
+  constant FUNC_SLT  : natural := 2#101010#;
+  constant FUNC_SLTU : natural := 2#101001#;
+  constant FUNC_JR   : natural := 2#001000#;
+  constant FUNC_JALR : natural := 2#001001#;
+  constant FUNC_SLL  : natural := 2#000000#;
+  constant FUNC_SRL  : natural := 2#000010#;
+  constant FUNC_SRA  : natural := 2#000011#;
+  constant FUNC_SLLV : natural := 2#000100#;
+  constant FUNC_SRLV : natural := 2#000110#;
+  constant FUNC_SRAV : natural := 2#000111#;
 
   --BRANCH CODES
-  constant BRANCH_BLTZ   : std_logic_vector (5 downto 0) := "000000";
-  constant BRANCH_BGEZ   : std_logic_vector (5 downto 0) := "000001";
-  constant BRANCH_BLTZAL : std_logic_vector (5 downto 0) := "010000";
-  constant BRANCH_BGEZAL : std_logic_vector (5 downto 0) := "010001";
+  constant BRANCH_BLTZ   : natural := 2#000000#;
+  constant BRANCH_BGEZ   : natural := 2#000001#;
+  constant BRANCH_BLTZAL : natural := 2#010000#;
+  constant BRANCH_BGEZAL : natural := 2#010001#;
 
 
   type rom_array is array (63 downto 0) of std_logic_vector(21 downto 0);  
@@ -120,40 +116,42 @@ architecture rom of mips_control is
   --signal dummy : integer := 61;
   --R Type ROM
   signal rom_r : rom_array := (
-    to_integer(unsigned(FUNC_JALR)) => "10100000------10001100",
-    others                          => ("10" & a_funct & "-------00000000")
+    FUNC_JALR => "10100000------10001100",
+    FUNC_JR   => "00------------10-----0",
+    others    => "10" & a_funct & "------00000000"
     );
 
 --Branch Type ROM
   signal rom_b : rom_array := (
-    to_integer(unsigned(BRANCH_BLTZ))   => "00100010010---11----10",
-    to_integer(unsigned(BRANCH_BGEZ))   => "00100010011---11----10",
-    to_integer(unsigned(BRANCH_BLTZAL)) => "10100010010---11111110",
-    to_integer(unsigned(BRANCH_BGEZAL)) => "10100010011---11111110"
+    BRANCH_BLTZ   => "00100010010---11----10",
+    BRANCH_BGEZ   => "00100010011---11----10",
+    BRANCH_BLTZAL => "10100010010---11111110",
+    BRANCH_BGEZAL => "10100010011---11111110",
+    others => "0000000000000000000000"
     );
 
 --All other instructions ROM
   signal rom_o : rom_array := (
-    to_integer(unsigned(OP_ADDI))  => "10100000------00010001",
-    to_integer(unsigned(OP_ADDIU)) => "10100001------00010001",
-    to_integer(unsigned(OP_ORI))   => "10100101------00010001",
-    to_integer(unsigned(OP_ANDI))  => "10001101------00010001",
-    to_integer(unsigned(OP_XORI))  => "10100110------00010001",
-    to_integer(unsigned(OP_SLTI))  => "10101010------00010001",
-    to_integer(unsigned(OP_SLTIU)) => "10101001------00010001",
-    to_integer(unsigned(OP_J))     => "00------------01------",
-    to_integer(unsigned(OP_JAL))   => "10------------01101100",
-    to_integer(unsigned(OP_JR))    => "00------------10-----0",
-    to_integer(unsigned(OP_JALR))  => "10100000------10001100",
-    to_integer(unsigned(OP_LB))    => "10100000---10100010101",
-    to_integer(unsigned(OP_LBU))   => "10100000---10000010101",
-    to_integer(unsigned(OP_LH))    => "10100000---01100010101",
-    to_integer(unsigned(OP_LHU))   => "10100000---01000010101",
-    to_integer(unsigned(OP_LW))    => "10100000---00100010101",
-    to_integer(unsigned(OP_SB))    => "01100000---10-00----01",
-    to_integer(unsigned(OP_SH))    => "01100000---01-00----01",
-    to_integer(unsigned(OP_SW))    => "01100000---00-00----01",
-    to_integer(unsigned(OP_LUI))   => "10------------000110-1"
+    OP_ADDI  => "10100000------00010001",
+    OP_ADDIU => "10100001------00010001",
+    OP_ORI   => "10100101------00010001",
+    OP_ANDI  => "10001101------00010001",
+    OP_XORI  => "10100110------00010001",
+    OP_SLTI  => "10101010------00010001",
+    OP_SLTIU => "10101001------00010001",
+    OP_J     => "00------------01------",
+    OP_JAL   => "10------------01101100",
+    OP_JALR  => "10100000------10001100",
+    OP_LB    => "10100000---10100010101",
+    OP_LBU   => "10100000---10000010101",
+    OP_LH    => "10100000---01100010101",
+    OP_LHU   => "10100000---01000010101",
+    OP_LW    => "10100000---00100010101",
+    OP_SB    => "01100000---10-00----01",
+    OP_SH    => "01100000---01-00----01",
+    OP_SW    => "01100000---00-00----01",
+    OP_LUI   => "10------------000110-1",
+    others   => "0000000000000000000000"
     );
 
   signal controlVector : std_logic_vector(21 downto 0);
@@ -163,17 +161,17 @@ begin
   with a_op select controlVector <=
     rom_r(to_integer(unsigned(a_funct)))    when "000000",
     rom_b(to_integer(unsigned('0' & a_rt))) when "000001",
-    rom_o(to_integer(unsigned(opcode)))     when others;
+    rom_o(to_integer(unsigned(a_op)))     when others;
 
   (
     o_RegWriteEnable,
     o_MemWriteEnable,
-    o_ALUFuntion(0),
-    o_ALUFuntion(1),
-    o_ALUFuntion(2),
-    o_ALUFuntion(3),
-    o_ALUFuntion(4),
-    o_ALUFuntion(5),
+    o_ALUFunction(0),
+    o_ALUFunction(1),
+    o_ALUFunction(2),
+    o_ALUFunction(3),
+    o_ALUFunction(4),
+    o_ALUFunction(5),
     o_BranchType(0),
     o_BranchType(1),
     o_BranchType(2),
@@ -186,7 +184,7 @@ begin
     o_RegWriteAddrSource(1),
     o_RegWriteDataSource(0),
     o_RegWriteDataSource(1),
-    o_ReadAddrSource,
+    o_RtReadAddrSource,
     o_ALUInputBSource
     )
     <= controlVector;
